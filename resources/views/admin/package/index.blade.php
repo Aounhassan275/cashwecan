4748796a8c51f697bd9b6db75e3238089647e1a8
Aounhassan275 <aounhassan275@gmail.com>
2022-01-02 14:42:25 +0500

    Fix Package Image Issue

--- resources/views/admin/package/index.blade.php
+++ resources/views/admin/package/index.blade.php
@@ -18,14 +18,15 @@
                         <th style="width:auto;">Sr#</th>
                         <th style="width:auto;">Package Name</th>
                         <th style="width:auto;">Package Price</th>
-                        <th style="width:auto;">Direct Income</th>
+                        {{-- <th style="width:auto;">Direct Income</th>
                         <th style="width:auto;">Direct Team Income</th>
                         <th style="width:auto;">Upline Income</th>
                         <th style="width:auto;">Down Line Income</th>
                         <th style="width:auto;">Upline Placement Income</th>
                         <th style="width:auto;">Down Line Placement Income</th>
                         <th style="width:auto;">Trade Income</th>
-                        <th style="width:auto;">Company Income</th>
+                        <th style="width:auto;">Company Income</th> --}}
+                        <th style="width:auto;">Image</th>
                         <th style="width:auto;">Max Limit</th>
                         <th style="width:auto;">Min Limit</th>
                         <th style="width:auto;">Action</th>
@@ -38,14 +39,15 @@
                         <td>{{$key+1}}</td>
                         <td>{{$package->name}}</td>
                         <td>{{$package->price}}</td>
-                        <td>{{$package->direct_income}}</td>
+                        <td><a href="{{asset($package->image)}}"><i class="feather text-info" data-feather="eye"></i></a></td>
+                        {{-- <td>{{$package->direct_income}}</td>
                         <td>{{$package->direct_team_income}}</td>
                         <td>{{$package->upline_income}}</td>
                         <td>{{$package->down_line_income}}</td>
                         <td>{{$package->upline_placement_income}}</td>
                         <td>{{$package->down_line_placement_income}}</td>
                         <td>{{$package->trade_income}}</td>
-                        <td>{{$package->company_income}}</td>
+                        <td>{{$package->company_income}}</td> --}}
                         <td>{{$package->max_limit}}</td>
                         <td>{{$package->min_limit}}</td>
                         <td class="table-action">
